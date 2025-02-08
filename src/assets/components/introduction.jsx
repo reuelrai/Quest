20f4cf9fe698a3954dba009ff7f8efedad0810a7
reuel.rai <reuelrai2001@gmail.com>
2025-02-08 15:15:41 +0545

    small changes

--- src/assets/components/introduction.jsx
+++ src/assets/components/introduction.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import "./quest.css"; // Import only here
+import { useNavigate } from "react-router-dom";
 
 function FirstQuest() {
+    const navigate = useNavigate();
     const [animate, setAnimate] = useState(false);
+    
 
     useEffect(() => {
         setTimeout(() => setAnimate(true), 50); // Slight delay for effect
@@ -11,8 +14,9 @@ function FirstQuest() {
     return (
         <div className="screen">
             <div className={animate ? "page-enter-active" : "page-enter"}>
-                <h1>Introduction</h1>
+                <h1>WELCOME</h1>
                 <p>Your adventure begins now!</p>
+                <button onClick={()=> navigate("/play")}>Next</button>
             </div>
         </div>
     );
---
+++ src/assets/components/playthrough.jsx
@@ -0,0 +1,12 @@
+
+function play(){
+    
+    return(
+        <div className="boxes">
+            <h1>this is div</h1>
+        </div>
+        
+    );
+}
+
+export default play
