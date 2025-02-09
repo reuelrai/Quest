5d244911871ec4eb1eca8a9908c0794fd4025ea8
reuel.rai <reuelrai2001@gmail.com>
2025-02-09 19:58:58 +0545

    Small Changes

--- src/assets/components/home.jsx
+++ src/assets/components/home.jsx
@@ -5,7 +5,7 @@ function home(){
         <div className="container">
             <h1>"1N73LL163NC3<br/>15<br/>7H3<br/>4B1L17Y<br/>70<br/>4D4P7<br/>70<br/>CH4NG3."</h1>
             <h1>- 573PH3N H4WK1NG</h1>
-            <button className="button" onClick={() => navigate("/quest")}>Proceed</button>
+            <button className="button" onClick={() => navigate("/quest")}>Next</button>
         </div>
     );
 }
