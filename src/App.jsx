f121ee762c9ec4339b444e977e52eedbec5ee1a2
reuel.rai <reuelrai2001@gmail.com>
2025-02-13 16:45:09 +0545

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from './assets/components/home.jsx';
 import Quest from "./assets/components/introduction.jsx";
-import Play from "./assets/components/playthrough.jsx"
+
 
 function App() {
   return(
@@ -11,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quest" element={<Quest />} />
-        <Route path="/play" element={<Play />} />
       </Routes>
     </Router>
     </>
