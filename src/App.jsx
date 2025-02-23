193d54be27280b46a94a166cc44581fa165fac09
reuel.rai <reuelrai2001@gmail.com>
2025-02-23 17:28:58 +0545

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,12 @@ import Card from "./assets/components/card.jsx";
 
 function App() {
   return (
-    <>
-      <div className="first-container">
-        <h1>Comments</h1>
-        <div className="second-container">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+    <><div className="container">
+      <h1>Comments</h1>
+      <div className="second-container">
+        <Card />
       </div>
+    </div>
     </>
 
   );
--- src/assets/components/card.jsx
+++ src/assets/components/card.jsx
@@ -2,10 +2,44 @@ import { Navigate } from "react-router-dom";
 
 function card(){
     return (
+        <>
         <div className="card">
             <p className="comment">Card Description</p>
             <p className="username">Card Title</p>
         </div>
+        <div className="card">
+            <p className="comment">Card Description</p>
+            <p className="username">Card Title</p>
+        </div>
+        <div className="card">
+            <p className="comment">Card Description</p>
+            <p className="username">Card Title</p>
+        </div>
+        <div className="card">
+            <p className="comment">Card Description</p>
+            <p className="username">Card Title</p>
+        </div>
+        <div className="card">
+            <p className="comment">Card Description</p>
+            <p className="username">Card Title</p>
+        </div>
+        <div className="card">
+            <p className="comment">Card Description</p>
+            <p className="username">Card Title</p>
+        </div>
+        <div className="card">
+            <p className="comment">Card Description</p>
+            <p className="username">Card Title</p>
+        </div>
+        <div className="card">
+            <p className="comment">Card Description</p>
+            <p className="username">Card Title</p>
+        </div>
+        <div className="card">
+            <p className="comment">Card Description</p>
+            <p className="username">Card Title</p>
+        </div>
+        </>
     )
 }
 
